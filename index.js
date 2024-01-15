515bae28f608277065cf197d05e89a7f35fb69f4
stoner <liyifan1@ft.tech>
2024-01-15 12:49:06 +0800

    3.12 - 3.18 ok

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
+
+const PhoneBook = require('./models/phonebook');
 
 app.use(cors())
 app.use(express.json());
@@ -13,75 +16,81 @@ morgan.token('body', function(req, res) {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-app.get('/api/persons', (request, response) => {
-    response.json(persons);
+app.get('/api/persons', (request, response, next) => {
+    PhoneBook.find({})
+        .then(books => {
+            response.json(books);
+        })
+        .catch(error => next(error))
 });
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const result = persons.find(person => person.id === id);
-
-    if (result) {
-        response.json(result);
-    } else {
-        response.status(404).end();
-    }
-
+app.get('/api/persons/:id', (request, response, next) => {
+    console.log(request.params.id)
+    PhoneBook.find({ _id: request.params.id })
+        .then(result => {
+            if (result) {
+                response.json(result);
+            } else {
+                response.status(404).end();
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
-    response.send(`Phonebook has info for ${persons.length} people <br> ${new Date()} `);
+app.get('/info', (request, response, next) => {
+    PhoneBook.find({})
+        .then(result => {
+        response.send(`Phonebook has info for ${result.length} people <br> ${new Date()} `);
+        })
+        .catch(error => next(error))
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    persons = persons.filter(person => person.id !== id);
-
-    response.status(204).end();
+app.delete('/api/persons/:id', (request, response, next) => {
+    PhoneBook.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 });
 
-app.post('/api/persons', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
+
+    const new_person = {
+        name: request.body.name,
+        number: request.body.number,
+    }
+
+    PhoneBook.findByIdAndUpdate(request.params.id, new_person, { new: true })
+        .then(updatePerson => {
+            response.json(updatePerson)
+        })
+        .catch(error => next(error))
+})
+
+app.post('/api/persons', (request, response, next) => {
     const new_person = request.body;
 
     if (new_person.name === undefined || new_person.number === undefined) {
         return response.status(400).json({ error: 'name or number is missing' });
     }
 
-    if (persons.find(person => person.name === new_person.name)) {
-        return response.status(400).json({ error: 'name must be unique' });
-    }
+    const new_phone_book = new PhoneBook({
+        name: new_person.name,
+        number: new_person.number,
+    })
 
-    const new_id = Math.floor(Math.random() * 1000000000);
-    new_person.id = new_id;
-    persons.push(new_person);
-
-    response.json(new_person);
+    new_phone_book.save()
+        .then(result => {
+            response.json(result);
+        })
+        .catch(error => next(error))
 })
 
-const PORT = process.env.PORT || 3001;
+app.use((error, request, response, next) => {
+    response.status(500).send(error)
+})
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
